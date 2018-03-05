08b0819c30db0c8e862a8d72a45658a454c1296d
vagrant <vagrant@fivecloud.local>
2018-03-05 14:43:09 +0000

    Release version 0.2.0

---
+++ recipes/server.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: apache
+# Recipe:: server
+#
+# Copyright (c) 2018 The Authors, All Rights Reserved.
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content "'<h1>Hello World!</h1>
+<h2> #{node['ipaddress']}</h2>
+<h2> #[node['hostname']}</h2>"
+
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
+
